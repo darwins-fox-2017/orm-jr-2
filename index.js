e903405fc11f750552a04cdd081240b0ef237330
Bambang Handoko <email": null>
2017-02-16 21:18:47 +0700

    upload file

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 "use strict"
 
+const repl = require('repl')
+
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+
+let playtime = process.argv
+let command = playtime.slice(2).join('')
+
+if (command == 'playtime') {
+  let dbmodel = new DBModel()
+
+  var r = repl.start('> ')
+  r.context.dbmodel = dbmodel
+  r.context.Student = Student
+  r.context.Cohort = Cohort
+}
--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,36 @@
 "use strict"
 
+const sqlite = require('sqlite3').verbose()
+
 class DBModel {
+  constructor() {
+    this.connection = new sqlite.Database('./db/student.db')
+  }
 
+  setup() {
+    let CREATE_TABLE_STUDENT = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL UNIQUE, lastname TEXT, cohort_id INTEGER, FOREIGN KEY (cohort_id) REFERENCES cohort(id))"
+    let CREATE_TABLE_COHORT = "CREATE TABLE IF NOT EXISTS cohorts (id INTEGER PRIMARY KEY AUTOINCREMENT, cohort_name TEXT UNIQUE)"
 
+    let db = this.connection
+    db.serialize(function() {
+      // Create TABLE
+      db.run(CREATE_TABLE_STUDENT, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Create Table Student Success");
+        }
+      })
+      db.run(CREATE_TABLE_COHORT, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Create Table Cohort Success");
+        }
+      })
+    })
+    return '';
+  }
 }
 
 export default DBModel
