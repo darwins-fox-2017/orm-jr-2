05e432bc4a984bcd81df362167506b714d69b602
Diky Arga <dikyarga.id@gmail.com>
2017-02-19 16:44:04 +0700

    Final release with Data generator for student and cohort using Faker. Added findAll (with paggination limit and offset) and findOrCreate() feature

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 "use strict"
-
+const repl = require('repl')
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+
+
+let dbModel = new DBModel('./db/student.db')
+// dbModel.createTable()
+// console.log(dbModel);
+
+let arg = process.argv;
+if (arg.length>2) {
+  if (arg[2].toLowerCase() == 'playtime') {
+     let r = repl.start(' > ')
+     r.context.dbModel = dbModel.dbName
+     r.context.createTable = dbModel.createTable
+     r.context.Student = Student
+     r.context.addStudent=Student.addStudent
+     r.context.updateStudent=Student.updateStudent
+     r.context.showAllStudent=Student.showAllStudent
+     r.context.findAll=Student.findAll
+     r.context.findOrCreate=Student.findOrCreate
+     r.context.generateFakeStudentData=Student.generateFakeStudentData
+     r.context.generateFakeCohortData=Cohort.generateFakeCohortData
+    //  Cohort
+     r.context.addCohort=Cohort.addCohort
+     r.context.showAllCohort=Cohort.showAllCohort
+     r.context.removeCohort=Cohort.removeCohort
+     r.context.seecohort=Cohort.seeAllCohort
+     r.context.help=Student.help
+
+  }
+}
--- models/cohort.js
+++ models/cohort.js
@@ -1,9 +1,48 @@
 "use strict"
+var faker = require('faker');
 
 import Student from "./student.js";
 
 class Cohort {
+  static addCohort(db, name){
+    let ADD_COHORT_QUERY = `INSERT INTO cohorts (name) VALUES (?)`
+    db.run(ADD_COHORT_QUERY, [name], function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`${name} succesfully added to the cohorts table`);
+        return true
+      }
+    })
+  }
 
+  static showAllCohort(db){
+    let SHOW_ALL_COHORT_QUERY = `SELECT * FROM cohorts`
+    db.each(SHOW_ALL_COHORT_QUERY, function(err, row){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(row);
+      }
+    })
+  }
+
+  static removeCohort(db, id){
+    let DELETE_COHORT_QUERY = `DELETE FROM cohorts WHERE id = ${id}`
+    db.run(DELETE_COHORT_QUERY, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Cohort with ${id} deleted`);
+      }
+    })
+  }
+
+  static generateFakeCohortData(db, totalNumber){
+    for (let i = 0; i < totalNumber; i++) {
+      Cohort.addCohort(db, faker.company.companyName())
+    }
+  }
 
 }
 
