d13c56e2c21b54276f39fe5b63b64c8f89cfb633
wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-02-22 15:18:59 +0700

    orm-jr-2

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 "use strict"
-
+//
 import DBModel from "./models/db_model.js";
-import Cohort from "./models/cohort.js";
-import Student from "./models/student.js";
+// import Cohort from "./models/cohort.js";
+// import Student from "./models/student.js";
+// const repl = require('repl');
+//
+// let db = new DBModel("./db/students.db")
+
+// let argv = process.argv[2]
+// if(argv == "playtime"){
+//   let r = repl.start('> ')
+//   r.context.dbModel = db
+//   r.context.Student = Student
+//   r.context.Cohort = Cohort
+// //
+console.log("ahahahahahahahhaa")
--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,24 @@
 "use strict"
+const sqlite = require('sqlite3').verbose()
 
-class DBModel {
+let CREATE_TABLE_STUDENTS= `CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT,firstname TEXT NOT NULL UNIQUE,lastname TEXT,cohort_id INTEGER NOT NULL, FOREIGN KEY(cohort_id) REFERENCES cohorts (id));`
+let CREATE_TABLE_COHORTS = `CREATE TABLE IF NOT EXISTS cohorts (id INTEGER PRIMARY KEY AUTOINCREMENT, cohort_name TEXT UNIQUE);`
 
-
+export default class DBModel {
+  constructor(file){
+    this.connection = new sqlite.Database(file)
+  }
+    setup(){
+      let connection = this.connection
+      connection.serialize(function(){
+        connection.run(CREATE_TABLE_STUDENTS, function(err){
+          err ? console.log(err):console.log("Create table students success!!");
+        })
+      })
+      connection.serialize(function(){
+        connection.run(CREATE_TABLE_COHORTS, function(err){
+          err ? console.log(err) : console.log("Create table cohorts success!!");
+        })
+      })
+    }
 }
-
-export default DBModel
